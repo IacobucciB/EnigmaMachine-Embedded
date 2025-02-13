7cc6a8679550f8b9ad22f878535ad3bf4aadae52
Lisandro Martinez <94946687+pickle1010@users.noreply.github.com>
2025-02-13 18:34:19 -0300

    Restaurar contenido correcto de animation.c ...
    comentandolo adecuadamente y realizando algunas
    correcciones menores.

--- enigma/inc/animation.h
+++ enigma/inc/animation.h
@@ -1,8 +1,16 @@
-/*
- * animation.h
+/**
+ * @file animation.h
+ * @brief LED matrix animation functions for displaying characters, numbers, and animations.
  *
- *  Created on: 2 feb. 2025
- *      Author: Lisandro
+ * Provides functions to initialize and control animations on an LED matrix
+ * using a MAX7219 driver.
+ *
+ * @author
+ *   - Juan Bautista Iacobucci <link00222@gmail.com>
+ *   - Fernando Ramirez Tolentino <fernandoramireztolentino@hotmail.com>
+ *   - Lisandro Martinez <lisandromartz@gmail.com>
+ * @date 2025-02-02
+ * @version 1.0
  */
 
 #ifndef ANIMATION_H_
@@ -10,13 +18,57 @@
 
 #include "sapi.h"
 
-void Animation_Init();
-void Animation_DrawCharacter(char c);
-void Animation_DrawNumber(uint8_t number);
-void Animation_DrawRomanNumber(uint8_t number);
-bool_t Animation_ShiftText(char* const text, bool_t reset);
-bool_t Animation_WaitInput();
-bool_t Animation_Loading(bool_t reset);
+/**
+ * @brief Initializes the animation system.
+ *
+ * Sets up the LED matrix and initializes the MAX7219 driver.
+ */
+void Animation_Init(void);
 
+/**
+ * @brief Displays a single character on the LED matrix.
+ *
+ * @param c Character to display.
+ */
+void Animation_DrawCharacter(char c);
+
+/**
+ * @brief Displays a two-digit number on the LED matrix.
+ *
+ * @param number The number to display (0-99).
+ */
+void Animation_DrawNumber(uint8_t number);
+
+/**
+ * @brief Displays a Roman numeral representation of a number (1-3).
+ *
+ * @param number The number to display.
+ */
+void Animation_DrawRomanNumber(uint8_t number);
+
+/**
+ * @brief Scrolls a text message across the LED matrix.
+ *
+ * @param text The message to display.
+ * @param reset If true, resets the scrolling animation.
+ * @return true if the text has finished scrolling, false otherwise.
+ */
+bool_t Animation_ShiftText(char* const text, bool_t reset);
+
+/**
+ * @brief Displays a waiting animation.
+ *
+ * @param reset If true, resets the animation.
+ * @return true when the animation cycle completes, false otherwise.
+ */
+bool_t Animation_WaitInput(bool_t reset);
+
+/**
+ * @brief Displays a loading animation.
+ *
+ * @param reset If true, resets the animation.
+ * @return true when the animation cycle completes, false otherwise.
+ */
+bool_t Animation_Loading(bool_t reset);
 
 #endif /* ANIMATION_H_ */
--- enigma/src/animation.c
+++ enigma/src/animation.c
@@ -1,209 +1,250 @@
-/*=============================================================================
- * Author: Martinez Lisandro <lisandromartz@gmail.com>
- * Date: 2025/01/10
- *===========================================================================*/
+ /**
+ * @file animation.c
+ * @brief LED matrix animation functions for displaying characters, numbers, and animations.
+ *
+ * Provides functions to initialize and control animations on an LED matrix
+ * using a MAX7219 driver.
+ *
+ * @author
+ *   - Juan Bautista Iacobucci <link00222@gmail.com>
+ *   - Fernando Ramirez Tolentino <fernandoramireztolentino@hotmail.com>
+ *   - Lisandro Martinez <lisandromartz@gmail.com>
+ * @date 2025-02-02
+ * @version 1.0
+ */
 
-/*=====[Inclusions of function dependencies]=================================*/
+#include "led_matrix.h"
+#include "font8x8_basic.h"
+ 
+#define CHAR_TO_IMAGE64x1_ROW( ROW, VALUE ) ( ((uint64_t) VALUE) << ((7 - ROW) * 8) )
+ 
+/*=====[Character Font Definitions]==========================================*/
 
-#include "sapi.h"
-#include "plugb.h"
-#include "rotary_encoder.h"
-#include "enigmaAPI.h"
-#include "PS2Keyboard.h"
-#include "animation.h"
-#include "MEF.h"
+/** Font for 5x3 pixel numbers */
+const uint8_t font5x3_numbers[10][8] = {
+    {0x00, 0xe0, 0xa0, 0xa0, 0xa0, 0xe0, 0x00, 0x00}, // 0
+    {0x00, 0xc0, 0x40, 0x40, 0x40, 0xe0, 0x00, 0x00}, // 1
+    {0x00, 0xe0, 0x20, 0xe0, 0x80, 0xe0, 0x00, 0x00}, // 2
+    {0x00, 0xe0, 0x20, 0x60, 0x20, 0xe0, 0x00, 0x00}, // 3
+    {0x00, 0xa0, 0xa0, 0xe0, 0x20, 0x20, 0x00, 0x00}, // 4
+    {0x00, 0xe0, 0x80, 0xe0, 0x20, 0xe0, 0x00, 0x00}, // 5
+    {0x00, 0xe0, 0x80, 0xe0, 0xa0, 0xe0, 0x00, 0x00}, // 6
+    {0x00, 0xe0, 0x20, 0x60, 0x20, 0x20, 0x00, 0x00}, // 7
+    {0x00, 0xe0, 0xa0, 0xe0, 0xa0, 0xe0, 0x00, 0x00}, // 8
+    {0x00, 0xe0, 0xa0, 0xe0, 0x20, 0x20, 0x00, 0x00}  // 9
+};
 
-/*=====[Definition macros of private constants]==============================*/
-#define NUM_ROTORS 3
-#define PLUGB_DELAY 500
-#define ROTOR_ANIM_DELAY 700
+/** Font for Roman numerals */
+const uint8_t RomanNumbers[3][8] = {
+    {0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18}, // I
+    {0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66}, // II
+    {0xDB, 0xDB, 0xDB, 0xDB, 0xDB, 0xDB, 0xDB, 0xDB}  // III
+};
 
-/* Pins used for data and clock from keyboard */
-#define IRQ_PIN  T_COL2
-#define DATA_PIN T_FIL1
+/** LED matrix instance */
+static ledMatrix_t mat;
 
-/*=====[Definitions of extern global variables]==============================*/
+/*=====[Function Implementations]============================================*/
 
-/*=====[Definitions of public global variables]==============================*/
-
-/*=====[Definitions of private global variables]=============================*/
-typedef enum { ENCRYPT, CONFIG_PB, CONFIG_ROTOR } MEF_state_t;
-static MEF_state_t state;
-
-static void MEF_ConfigRotor();
-static void MEF_Encrypt();
-static void MEF_ConfigPB();
-static void (*MEF_Behavior[])(void) = { MEF_Encrypt, MEF_ConfigPB, MEF_ConfigRotor };
-
-static delay_t plugbDelay;
-static delay_t rotorAnimDelay;
-static bool_t keyPressed = false;
-static bool_t pressMsgDone = false;
-static uint8_t waitAnimTimes = 3;
-static bool_t loadAnimDone = true;
-static bool_t rotorAnimDone = false;
-
-static char* const plugbMessage = "PLUG ";
-static char* const encryptMessage = "PRESS A KEY ";
-
-static uint8_t rotorIndex = 0;
-static uint8_t rotorPos[NUM_ROTORS] = { 0 };
-
-static char out;
-
-void MEF_Init()
-{
-	PLUGB_Init();
-
-	RotaryEncoder_Init();
-	delayInit( &rotorAnimDelay, 500 );
-
-	PS2Keyboard_Init(DATA_PIN, IRQ_PIN, 0);
-
-	Animation_Init();
-
-	state = ENCRYPT;
-	out = 0;
-	enigma_init( 3, 2, 1, rotorPos[0], rotorPos[1], rotorPos[2] );
-	Animation_WaitInput(true);
-	pressMsgDone = Animation_ShiftText(encryptMessage, true);
+/**
+ * @brief Initializes the animation system.
+ *
+ * Sets up the LED matrix and initializes the MAX7219 driver.
+ */
+void Animation_Init() {
+    max7219_t max7219;
+    Max7219Init(&max7219, ENET_RXD1, max7219_spi_default_cfg);
+    MatrixInit(&mat, max7219, ROT_270_CW);
 }
 
-void MEF_Update(void) {
-	Animation_Loading(true);
-	loadAnimDone = false;
-
-	if( state == CONFIG_ROTOR && rotorIndex != (NUM_ROTORS - 1) )
-	{
-		rotorIndex++;
-	}
-	else
-	{
-		if( state == CONFIG_ROTOR ) {
-			rotorIndex = 0;
-		}
-		else if ( state == ENCRYPT )
-		{
-			PS2Keyboard_DisableInt();
-		}
-		state++;
-		state %= 3;
-	}
-
-	switch ( state ) {
-		case ENCRYPT:
-			out = 0;
-			enigma_init( 3, 2, 1, rotorPos[0], rotorPos[1], rotorPos[2] );
-			PS2Keyboard_EnableInt();
-			keyPressed = false;
-			Animation_WaitInput(true);
-			pressMsgDone = Animation_ShiftText(encryptMessage, true);
-			printf( "Modo encriptacion \r\n" );
-			break;
-		case CONFIG_PB:
-			Animation_ShiftText(plugbMessage, true);
-			delayInit( &plugbDelay, PLUGB_DELAY );
-			printf( "Configurando plugboard \r\n" );
-			break;
-		case CONFIG_ROTOR:
-			delayInit( &rotorAnimDelay, ROTOR_ANIM_DELAY );
-			rotorAnimDone = false;
-			printf( "Configurando rotor %d \r\n", rotorIndex + 1);
-			break;
-	}
+/**
+ * @brief Displays a single character on the LED matrix.
+ *
+ * @param c Character to display.
+ */
+void Animation_DrawCharacter(char c) {
+    uint64_t image = 0x00;
+    for (uint8_t i = 0; i < 8; ++i) {
+        image |= CHAR_TO_IMAGE64x1_ROW(i, font8x8_ib8x8u[c][i]);
+    }
+    MatrixSetImage(&mat, image);
+    MatrixUpdate(&mat);
 }
 
-static void MEF_Encrypt()
-{
-	if ( PS2Keyboard_Available() )
-	{
-		keyPressed = true;
-		uint16_t c = PS2Keyboard_Read();
-		if (c > 0) {
-			printf("Value ");
-			if ('A' <= c && c <= 'Z')
-			{
-				printf("%c", c);
-			}
-			else {
-				printf("%x", c);
-			}
-			printf(" - Status Bits ");
-			printf("%x", c >> 8);
-			printf("  Code ");
-			printf("%x", c & 0xFF);
+/**
+ * @brief Displays a two-digit number on the LED matrix.
+ *
+ * @param number The number to display (0-99).
+ */
+void Animation_DrawNumber(uint8_t number) {
+    uint8_t digit0 = number % 10;
+    uint8_t digit1 = number / 10;
+    uint64_t image = 0x00;
 
-			if ('A' <= c && c <= 'Z')
-			{
-				out = encrypt_char( PLUGB_GetMapping(c) );
-				out = PLUGB_GetMapping(out);
-				printf(" - out : %c", out);
-				Animation_DrawCharacter(out);
-			}
+    for (uint8_t i = 1; i < 6; ++i) {
+        image |= CHAR_TO_IMAGE64x1_ROW(i, font5x3_numbers[digit1][i]);
+        image |= CHAR_TO_IMAGE64x1_ROW(i, font5x3_numbers[digit0][i]) >> 4;
+    }
 
-			printf("\r\n");
-		}
-	}
-	else if ( !keyPressed )
-	{
-		if ( !pressMsgDone )
-		{
-			pressMsgDone = Animation_ShiftText(encryptMessage, false);
-		}
-		else
-		{
-			if ( Animation_WaitInput(false) )
-			{
-				--waitAnimTimes;
-				if ( waitAnimTimes == 0 )
-				{
-					waitAnimTimes = 3;
-					Animation_WaitInput(true);
-					pressMsgDone = false;
-				}
-			}
-		}
-	}
+    MatrixSetImage(&mat, image);
+    MatrixUpdate(&mat);
 }
 
-static void MEF_ConfigPB()
-{
-	if ( delayRead( &plugbDelay ) )
-	{
-		PLUGB_Scan();
-		printf( "Plugboard: %s \r\n", PLUGB_GetAllMappings() );
-	}
-	Animation_ShiftText(encryptMessage, false);
-}
-
-static void MEF_ConfigRotor()
-{
-	if ( !rotorAnimDone && !delayRead(&rotorAnimDelay) ) {
-		Animation_DrawRomanNumber(rotorIndex + 1);
-		return; // No seguimos si no se mostró lo suficiente el nro de rotor
-	}
-	else
-	{
-		rotorAnimDone = true;
-		Animation_DrawNumber(rotorPos[rotorIndex] + 1);
-	}
-
-    int8_t delta = RotaryEncoder_Read_Blocking();
-    if (delta != 0) {
-        int8_t newPos = rotorPos[rotorIndex] + delta;
-
-        // Asegurar que el nuevo valor esté dentro de los límites
-        if (newPos >= 0 && newPos <= 25) {
-            rotorPos[rotorIndex] = newPos;
-            Animation_DrawNumber(newPos + 1);
+/**
+ * @brief Displays a Roman numeral representation of a number (1-3).
+ *
+ * @param number The number to display.
+ */
+void Animation_DrawRomanNumber(uint8_t number) {
+    number %= 4;
+    if (number != 0) {
+        uint64_t image = 0x00;
+        for (uint8_t i = 0; i < 8; ++i) {
+            image |= CHAR_TO_IMAGE64x1_ROW(i, RomanNumbers[number - 1][i]);
         }
+        MatrixSetImage(&mat, image);
+        MatrixUpdate(&mat);
     }
 }
 
-void MEF_Run()
+/**
+ * @brief Scrolls a text message across the LED matrix.
+ *
+ * @param text The message to display.
+ * @param reset If true, resets the scrolling animation.
+ * @return true if the text has finished scrolling, false otherwise.
+ */
+bool_t Animation_ShiftText(char* const text, bool_t reset) {
+    static uint8_t frame = 0;
+    static uint64_t image = 0x00;
+    static delay_t frameDelay;
+    static char* message;
+    static char current_char = 0x00;
+
+    if (reset) {
+        frame = 0;
+        message = text;
+        current_char = message[0];
+        image = 0x00;
+        delayInit(&frameDelay, 75);
+    }
+
+    if (delayRead(&frameDelay)) {
+        image <<= 1;
+        image &= 0xfefefefefefefefe;
+
+        for (uint8_t i = 0; i < 8; ++i) {
+            uint8_t byte = (font8x8_ib8x8u[current_char][i] >> (7 - frame % 8)) & 0x01;
+            image |= CHAR_TO_IMAGE64x1_ROW(i, byte);
+        }
+
+        MatrixSetImage(&mat, image);
+        MatrixUpdate(&mat);
+
+        frame++;
+        if (frame % 8 == 0) {
+            current_char = message[frame / 8];
+            if (current_char == '\0') {
+                frame = 0;
+                current_char = message[0];
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+/**
+ * @brief Displays a waiting animation.
+ *
+ * @param reset If true, resets the animation.
+ * @return true when the animation cycle completes, false otherwise.
+ */
+bool_t Animation_WaitInput(bool_t reset) {
+    static uint8_t frame = 0;
+    static uint64_t image = 0x1054381000000038;
+    static delay_t frameDelay;
+
+    if (reset) {
+        frame = 0;
+        image = 0x1054381000000038;
+        delayInit(&frameDelay, 180);
+    }
+
+    if (delayRead(&frameDelay)) {
+        switch ( frame ) {
+            case 0:
+                image = 0x1054381000000038;
+                break;
+            case 1:
+                image = 0x1010543810000038;
+                break;
+            case 2:
+                image = 0x1010105438100038;
+                break;
+            case 3:
+                image = 0x0010105438100038;
+                break;
+            case 4:
+                image = 0x000010105438107c;
+                break;
+            case 5:
+                image = 0x00001010543810fe;
+                break;
+        }
+
+        MatrixSetImage(&mat, image);
+        MatrixUpdate(&mat);
+        frame = (frame + 1) % 6;
+        return frame == 5;
+    }
+
+    return false;
+}
+
+/**
+ * @brief Displays a loading animation.
+ *
+ * @param reset If true, resets the animation.
+ * @return true when the animation cycle completes, false otherwise.
+ */
+bool_t Animation_Loading(bool_t reset)
 {
-	if ( loadAnimDone )
-		(*MEF_Behavior[ state ])();
-	else
-		loadAnimDone = Animation_Loading(false);
+    static uint8_t frame = 0;
+    static uint8_t i = 2;
+    static delay_t frameDelay;
+
+    if ( reset ) {
+        frame = 0;
+        i = 2;
+        delayInit(&frameDelay, 100);
+    }
+
+    if ( delayRead(&frameDelay) )
+    {
+        switch (frame) {
+            case 0:
+                MatrixSetImage(&mat, 0x0018181818181800);
+                break;
+            case 1:
+                MatrixSetImage(&mat, 0x0002040810204000);
+                break;
+            case 2:
+                MatrixSetImage(&mat, 0x7ffe000000);
+                break;
+            case 3:
+                MatrixSetImage(&mat, 0x0040201008040200);
+                i--;
+                break;
+            default:
+                break;
+        }
+        MatrixUpdate(&mat);
+
+        frame = (frame + 1) % 4;
+        return i == 0;
+    }
+
+    return false;
 }
